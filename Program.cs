4659b1f9d548e05cc6734799bcb5f51cbc42b3b7
alex <alexandr.paramon56@gmail.com>
2025-06-24 18:05:28 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_object_mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
